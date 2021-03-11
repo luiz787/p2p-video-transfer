8d6cba0d22ce57827d6d23198797fb33619d1b73
Luiz Berto <diasbertoluiz@gmail.com>
2021-03-11 18:27:54 -0300

    implements timeout logic

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,6 +5,7 @@ use std::{
     env,
     io::ErrorKind,
     net::{SocketAddr, UdpSocket},
+    time::{Duration, Instant},
 };
 
 mod client_config;
@@ -19,9 +20,9 @@ fn main() {
     let udp_socket = UdpSocket::bind(("127.0.0.1", 0)).unwrap();
 
     // TODO: refactor to use nonblocking IO
-    /*udp_socket
-    .set_nonblocking(true)
-    .expect("Failed to set socket to nonblocking mode.");*/
+    udp_socket
+        .set_nonblocking(true)
+        .expect("Failed to set socket to nonblocking mode.");
 
     udp_socket
         .connect(config.address)
@@ -31,18 +32,12 @@ fn main() {
         .expect("Falha ao enviar mensagem");
 
     let mut all_chunks_received = false;
+    let start = Instant::now();
 
     // TODO: change condition to: while did not receive all chunks or not timeout
-    while !all_chunks_received {
-        println!("Missing chunks:");
-
-        for (c, received) in control_map.iter() {
-            if !received {
-                println!("{}", c);
-            }
-        }
-
+    while !all_chunks_received && !timed_out(&start) {
         let mut buffer = [0; 60 * 1024];
+
         let result = udp_socket.recv_from(&mut buffer);
         match result {
             Ok((bytes_read, remote_addr)) => {
@@ -62,12 +57,27 @@ fn main() {
             Err(ref err) if err.kind() != ErrorKind::WouldBlock => {
                 panic!("Failed to read from udp socket");
             }
-            _ => {}
+            _ => {
+                continue;
+            }
         }
 
         all_chunks_received = control_map.iter().all(|(_chunk, received)| *received);
         println!("All received? {}", all_chunks_received);
     }
+
+    println!("Exiting...");
+}
+
+fn timed_out(start: &Instant) -> bool {
+    let time_elapsed = Instant::now() - *start;
+    let timed_out = time_elapsed > Duration::from_secs(5);
+
+    if timed_out {
+        println!("Timed out");
+    }
+
+    timed_out
 }
 
 fn create_control_map(config: &ClientConfig) -> HashMap<u16, bool> {
